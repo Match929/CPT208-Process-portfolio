37212998e80049e48f4ccb96b8beea4c355c206b
Minghao <1848339438@qq.com>
2026-05-10 13:27:57 +0800

    upadte

--- src/pages/UserManual.jsx
+++ src/pages/UserManual.jsx
@@ -7,7 +7,7 @@ const UserManual = () => {
         <h3 className="section-title"><i className="fa-solid fa-book-open"></i> User Manual</h3>
         <p className="section-sub">Step-by-step guide to getting started with Climblink</p>
         <p style={{ textAlign: 'center', marginTop: '40px', color: 'var(--gray)' }}>
-          Content will be added later...
+          Content will be added later
         </p>
       </div>
     </section>
